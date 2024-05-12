716c8f9eab2de50b39a78341fe42c86936b3c3c8
Erik Xu <81636759@qq.com>
2024-05-12 23:19:13 +0800

    Add log api.

---
+++ src/WebApi/Controllers/LogsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LogsController : ControllerBase
+    {
+        private readonly ILogger<LogsController> _logger;
+
+        public LogsController(ILogger<LogsController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost("apisix")]
+        public async Task<IActionResult> SaveApisixLog()
+        {
+            using (var reader = new StreamReader(HttpContext.Request.Body))
+            {
+                var postData = await reader.ReadToEndAsync();
+                _logger.LogInformation(postData);
+            }
+            return Ok();
+        }
+    }
+}
